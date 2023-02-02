58a5ab0ce6236eba4c34ba73d9789bb126554fed
Pablo Berry <pawel.borowiec@tutanota.com>
2023-02-02 18:32:39 +0100

    NOW IT WORKS!

--- netlify/functions/fetchApi.ts
+++ netlify/functions/fetchApi.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 require('dotenv').config()
 
-exports.handler = async function (event) {
+exports.handler = async function (event, context, callback) {
     const city = event.queryStringParameters.city
-    const apiKey = process.env.weatherKEY
+    const apiKey = '0a7876f173f8046b41756fe449f3deef'
 
     const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
 
@@ -14,18 +14,14 @@ exports.handler = async function (event) {
             `https://api.openweathermap.org/data/3.0/onecall?lat=${geoResponse.data[0].lat}&lon=${geoResponse.data[0].lon}&units=metric&appid=${apiKey}`,
         )
 
-        // const response = {
-        //     ...data,
-        //     ...geoResponse.data,
-        // }
-
-        JSON.stringify(data)
-
-        console.log(data)
+        const response = {
+            ...data,
+            ...geoResponse.data,
+        }
 
         return {
             statusCode: 200,
-            body: data,
+            body: JSON.stringify(response),
         }
     } catch (error) {
         const { status, statusText, headers, data } = error.response
