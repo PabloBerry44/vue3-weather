a614e23c39391bcabc19e84f439107c88c573bbf
Pablo Berry <pawel.borowiec@tutanota.com>
2023-02-02 15:58:51 +0100

    return geoResponse

--- netlify/functions/callAPI.ts
+++ netlify/functions/callAPI.ts
@@ -16,7 +16,7 @@ exports.handler = async function (event, context, callback) {
 
         return {
             statusCode: 200,
-            body: JSON.stringify(data) + JSON.stringify(geoResponse),
+            body: JSON.stringify(data, geoResponse),
         }
     } catch (error) {
         const { status, statusText, headers, data } = error.response
