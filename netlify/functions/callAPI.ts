adebdafcb295cc801729d1ed610cab20340b1ecf
Pablo Berry <pawel.borowiec@tutanota.com>
2023-02-02 16:14:20 +0100

    object assign

--- netlify/functions/callAPI.ts
+++ netlify/functions/callAPI.ts
@@ -10,13 +10,13 @@ exports.handler = async function (event, context, callback) {
     try {
         const geoResponse = await axios.get(geoURL)
 
-        const data = await axios.get(
+        const { data } = await axios.get(
             `https://api.openweathermap.org/data/3.0/onecall?lat=${geoResponse.data[0].lat}&lon=${geoResponse.data[0].lon}&units=metric&appid=${apiKey}`,
         )
 
         return {
             statusCode: 200,
-            body: JSON.stringify(data),
+            body: JSON.stringify(Object.assign(data, geoResponse)),
         }
     } catch (error) {
         const { status, statusText, headers, data } = error.response
